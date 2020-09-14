a2123460464eb1f22c3aa31745da8862e90db85d
nathanbaleeta <nbaleeta@gmail.com>
2020-09-14 18:00:46 +0300

    Added frontend template structure based on preact js

---
+++ frontend/src/components/header/index.js
@@ -0,0 +1,19 @@
+import { h } from "preact";
+import { Link } from "preact-router/match";
+import style from "./style.css";
+
+const Header = () => (
+  <header class={style.header}>
+    <h1>IoGT</h1>
+    <nav>
+      <Link activeClassName={style.active} href="/">
+        Home
+      </Link>
+      <Link activeClassName={style.active} href="/profile">
+        Me
+      </Link>
+    </nav>
+  </header>
+);
+
+export default Header;
