905ba4898327ded824e1043b320cf7a77cb5b66e
Jamila Toaha <jtoahacoding@gmail.com>
2020-10-07 03:17:32 -0400

    (fix) matched wireframes more closely (feat) added language dropdown menus

--- frontend/src/components/header/header-top.js
+++ frontend/src/components/header/header-top.js
@@ -2,46 +2,37 @@ import { h } from 'preact'
 import { Link } from 'preact-router/match'
 import style from './style.css'
 
+import LanguageDropdownMenu from './language-dropdown-menu'
 import SearchBar from './searchbar'
 
-const HeaderTop = () => {
-  let currentLanguage = 'English' //ideally this would be a prop sent in
-  let languageList = [
-    'English',
-    'العربيّة',
-    'FRANÇAIS',
-    'ESPAÑOL',
-    'PORTUGUÊS',
-    'РУССКИЙ',
-  ]
+const HeaderTop = (props) => {
+  let languageList = props.languageList
+  let currentLanguage = props.currentLanguage //ideally this would be a prop sent in
+
+  let signedIn = false //default value to be replaced
+  let joinStatus = signedIn ? 'My Profile' : 'Join'
+
   return (
     <div class={style['header-top']}>
       {/* Only on Feature Phone: logo-small, current-language, change-language */}
-      <div class={style['header-grid']}></div>
-      <div class={style['header-top-language']}>
-        <div class={style['header-language-brief']}>
-          <div class={style['current-language']}>
-            Current Language: <span>{currentLanguage}</span>
-          </div>
-          <div class={style['change-language']}>
-            <input
-              type='checkbox'
-              class={style.collapse}
-              id='handel1'
-            />
-            <label for='handel1'>
-              Change Language <i class='material-icons'>keyboard_arrow_down</i>
-            </label>
-            <div class={style['content']}>
-              <div class={style['language-list-menu-content']}>
-                <ul>
-                  {languageList.map((language) => (
-                    <li id={language} class={style.languageListItem}>
-                      <Link>{language}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+      <div class={style['header-top-language-strip']}>
+        <div class={style['current-language']}>
+          Language: <span>{currentLanguage}</span>
+        </div>
+        <div class={style['change-language']}>
+          <input type='checkbox' class={style.collapse} id='handel1' />
+          <label for='handel1'>
+            Change Language <i class='material-icons'>keyboard_arrow_down</i>
+          </label>
+          <div class={style['content']}>
+            <div class={style['language-list-menu-content']}>
+              <ul>
+                {languageList.map((language) => (
+                  <li id={language} class={style.languageListItem}>
+                    <Link>{language}</Link>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
@@ -55,42 +46,45 @@ const HeaderTop = () => {
           <img src='/assets/icons/Logo-with-text.png' />
         </Link>
         <div class={style['join-menu']}>
-          <Link>Join </Link>
+          <Link>{joinStatus} </Link>
           <Link>Menu </Link>
         </div>
 
-
         <div class={style['signin-language']}>
-        <Link
-        class={style['signin']}
-        activeClassName={style.active}
-        href='/signin'
-      >
-        Sign in
-      </Link>
-          <select>
-            <option value="English" >English</option>
-                  {languageList.map(language => <option value={language}>{language}</option>)}
-          </select>
+          <Link
+            class={style['signin']}
+            activeClassName={style.active}
+            href='/signin'
+          >
+            Sign in
+          </Link>
+          <LanguageDropdownMenu
+            currentLanguage={currentLanguage}
+            languageList={languageList}
+          />
         </div>
         <div class={style['search-menu']}>
-          <Link><icon class="material-icons">search</icon></Link>
-          <Link><icon class="material-icons">menu</icon></Link>
+          <Link>
+            <icon class='material-icons'>search</icon>
+          </Link>
+          <Link>
+            <icon class='material-icons'>menu</icon>
+          </Link>
         </div>
       </div>
+
       <SearchBar />
       {/* Sign in is placed near footer on Feature Phone */}
-    <div class={style['tabletDesktopSignin']}>
-         <Link
-        class={style['signin']}
-        activeClassName={style.active}
-        href='/signin'
-      >
-         <img src="../../assets/icons/nav-icons/profile-icon-green.png"/>
-            Sign out
-      </Link>
-
-    </div>
+      <div class={style['tabletDesktopSignin']}>
+        <Link
+          class={style['signin']}
+          activeClassName={style.active}
+          href='/signin'
+        >
+          <img src='../../assets/icons/nav-icons/profile-icon-green.png' />
+          Sign out
+        </Link>
+      </div>
     </div>
   )
 }
