e5614993588d4ea365b0c8af36146568df74b147
Lawrence Lin <lawlin@cs.stonybrook.edu>
2021-02-09 18:06:53 -0500

    Refactored JS to TS

--- frontend/-refactored-frontend/frontendts/src/components/app.tsx
+++ frontend/-refactored-frontend/frontendts/src/components/app.tsx
@@ -6,18 +6,29 @@ import Profile from '../routes/profile';
 import NotFoundPage from '../routes/notfound';
 import Header from './header';
 
+import Section from '../routes/sections';
+
 const App: FunctionalComponent = () => {
-    return (
-        <div id="app">
-            <Header />
-            <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
-        </div>
-    );
+  return (
+    <div id='app'>
+      <link
+        href='https://fonts.googleapis.com/icon?family=Material+Icons'
+        rel='stylesheet'
+      />
+      <div id='content-wrap'>
+        <Header />
+        <Router>
+          <Home path='/' />
+          <Profile path='/profile/' user='me' />
+          <Profile path='/profile/:user' />
+          <Section path='/section/' />
+          <Section path='/section/:section' />
+          <Section path='/section/:section/:topic' />
+          <NotFoundPage default />
+        </Router>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- frontend/-refactored-frontend/frontendts/src/routes/home/index.tsx
+++ frontend/-refactored-frontend/frontendts/src/routes/home/index.tsx
@@ -1,13 +1,13 @@
 import { FunctionalComponent, h } from 'preact';
+import ArticlesView from '../../components/articles/articlesView';
 import style from './style.css';
 
 const Home: FunctionalComponent = () => {
-    return (
-        <div class={style.home}>
-            <h1>Home</h1>
-            <p>This is the Home component.</p>
-        </div>
-    );
+  return (
+    <div class={style.home}>
+      <ArticlesView />
+    </div>
+  );
 };
 
 export default Home;
